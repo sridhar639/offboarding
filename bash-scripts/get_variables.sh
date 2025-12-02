e49a23c0c82d06d719f907d6bfd7e247d8f4dfe8
Sridhar Ramasamy <Sridhar.Ramasamy@cdw.com>
2025-12-02 18:44:23 +0530

    Testv3

--- bash-scripts/get_variables.sh
+++ bash-scripts/get_variables.sh
@@ -1,6 +1,9 @@
 offboarding_variable_file="cloudformation/offboarding_variables.yaml"
+ignore_list_file="cloudformation/ignore_list.yaml"
 
 account_no=$(yq -r '.AccountNo.Value' "$offboarding_variable_file")
+
+ignore_list=$(yq -r '.Ignore.Value' "$ignore_list_file")
 
 cdw_master_org_role="CDWMasterOrgAdminRole"
 
@@ -20,6 +23,7 @@ export cdw_master_org_role="$cdw_master_org_role"
 export cdw_offboarding_role="$cdw_offboarding_role"
 export bluemoon_account="$bluemoon_account"
 export SEARCH="$SEARCH"
+export ignore_list="$ignore_list"
 EOL
 
 echo -e "\nWrote Everything to /tmp/build_vars.sh"
